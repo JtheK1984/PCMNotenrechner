b55c36462dcef2685a48f2fb1af2e14c1a3757fd
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-07-28 13:14:54 +0200

    Version 1.0

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,10 +1,13 @@
-echo "Kopiere Datei ins Setupverzeichnis"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
 copy /y /v Win32\Release\PCMNotenrechner.exe "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
+copy /y /v Win32\Release\PCMNotenrechner.DE "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
+copy /y /v Win32\Release\PCMNotenrechner.EN "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
 copy /y /v Win64\Release\PCMNotenrechner.exe "e:\Inno\Setupfiles\Programme\PCMNotenrechner_x64"
-
 copy /y /v Win64\Release\PCMNotenrechner.DE "e:\Inno\Setupfiles\Programme\PCMNotenrechner_x64"
 copy /y /v Win64\Release\PCMNotenrechner.EN "e:\Inno\Setupfiles\Programme\PCMNotenrechner_x64"
 
+echo "Kopiere Doku ins Setupverzeichnis"
 copy /y /v PCMNotenrechner.docx "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
 copy /y /v PCMNotenrechner.pdf "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
 copy /y /v PCMNotenrechner.htm "e:\Inno\Setupfiles\Programme\PCMNotenrechner"
